658e7e8295618b2b149da14c3071ea9badc10071
xoanle <97075343+xoanle@users.noreply.github.com>
2023-08-16 09:48:58 +0700

    [ADD] add framework core

---
+++ automated-test/src/main/java/com/auto/data/enums/Navigation.java
@@ -0,0 +1,19 @@
+package com.auto.data.enums;
+
+public enum Navigation {
+    HOME("Home"),
+    INVOICES("Invoices"),
+    SETTINGS("Settings"),
+    ITEMS("Items"),
+    EXPENSES("Expenses");
+
+    private String value;
+
+    Navigation(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
